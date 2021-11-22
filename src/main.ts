75df1915df9d78ccee042a57aeaae847b12b5465
dong-woogie <com6511@gmail.com>
2021-11-22 16:52:06 +0900

    check log in production env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('process env is ');
+  console.log(process.env);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();

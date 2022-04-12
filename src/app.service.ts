b0e19eebefd4613cd7a7384c04305b60f0376709
dong-woogie <com6511@gmail.com>
2022-04-13 03:18:59 +0900

    deploy simple app

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getApi(): string {
+    return 'welcome api';
+  }
+
+  getEnv() {
+    return {
+      NODE_ENV: process.env.NODE_ENV,
+      DB_NAME: process.env.DB_NAME,
+      DB_PORT: process.env.DB_PORT,
+    };
+  }
 }

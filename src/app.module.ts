235deca91b36028e56af3395c38a9612e1ef3708
dong-woogie <com6511@gmail.com>
2022-04-14 01:48:18 +0900

    check env after deploy ecs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string(),
+        DB_NAME: Joi.string(),
+        DB_PORT: Joi.string(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
